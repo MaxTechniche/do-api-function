014ae162e01a5c596651afe866b87b5024cf26cc
Jacob <max.techniche@gmail.com>
2022-10-20 18:18:00 -0500

    make sure connection closes

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import uvicorn
 import os
 import dotenv
 import psycopg2
-from requests import get
+import urllib.request
 
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
@@ -15,37 +15,43 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="localhost")
 
 
 class SETTINGS:
-    POSTGRES_USER: str = os.getenv("user")
-    POSTGRES_PASSWORD = os.getenv("password")
-    POSTGRES_HOST: str = os.getenv("host")
-    POSTGRES_PORT: str = os.getenv("port")
-    POSTGRES_DB: str = os.getenv("database")
-    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    POSTGRES_USER: str = os.getenv("USER")
+    POSTGRES_PASSWORD = os.getenv("PASSWORD")
+    POSTGRES_HOST: str = os.getenv("HOST")
+    POSTGRES_PORT: str = os.getenv("PORT")
+    POSTGRES_DB: str = os.getenv("DATABASE")
     DATABASE_QUERY: str = os.getenv("QUERY")
+    TOKEN: str = os.getenv("TOKEN")
 
 
 settings = SETTINGS()
 
 
 def token_authorized(token):
-    return token == os.environ.get("TOKEN")
+    return token == settings.TOKEN
 
 
 def get_data():
-    conn = psycopg2.connect(
-        database=settings.POSTGRES_DB,
-        host=settings.POSTGRES_HOST,
-        user=settings.POSTGRES_USER,
-        password=settings.POSTGRES_PASSWORD,
-        port=settings.POSTGRES_PORT,
-    )
-    query = settings.DATABASE_QUERY
-    cur = conn.cursor()
-    cur.execute(query)
-    data = cur.fetchall()
-    cur.close()
-    conn.close()
-    return {"data": data}
+    try:
+        conn = psycopg2.connect(
+            database=settings.POSTGRES_DB,
+            host=settings.POSTGRES_HOST,
+            user=settings.POSTGRES_USER,
+            password=settings.POSTGRES_PASSWORD,
+            port=settings.POSTGRES_PORT,
+        )
+        query = settings.DATABASE_QUERY
+        cur = conn.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+        return {"data": data, "error": None}
+    finally:
+        try:
+            cur.close()
+            conn.close()
+        except UnboundLocalError:
+            return {"data": None, "error": "Error with connection to db"}
+        
 
 @app.get("/")
 async def read_items(token: str = Depends(oauth2_scheme)):
@@ -60,5 +66,6 @@ async def get_ip(token: str = Depends(oauth2_scheme)):
     if not token_authorized(token):
         raise HTTPException(status.HTTP_401_UNAUTHORIZED)
 
-    ip = get('https://api.ipify.org').content.decode('utf8')
-    return {"The IP address for this function is": ip}
+    external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+
+    return {"data": external_ip, "error": "ip request"}
